a653fb615cdd0283faee8c13d22202d3e5e11450
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-03-29 03:51:19 +0000

    deploy: 3c28cf193b60b3b7ff0a9b5038a1a60830edc49f

---
+++ trait.impl/pyo3/impl_/pyclass/trait.PyClassNewTextSignature.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mega_py",[["impl PyClassNewTextSignature&lt;<a class=\"struct\" href=\"mega_py/struct.Client.html\" title=\"struct mega_py::Client\">Client</a>&gt; for PyClassImplCollector&lt;<a class=\"struct\" href=\"mega_py/struct.Client.html\" title=\"struct mega_py::Client\">Client</a>&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[285]}
---
+++ trait.impl/pyo3/pyclass/trait.PyClass.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mega_py",[["impl PyClass for <a class=\"struct\" href=\"mega_py/struct.Client.html\" title=\"struct mega_py::Client\">Client</a>"],["impl PyClass for <a class=\"struct\" href=\"mega_py/struct.File.html\" title=\"struct mega_py::File\">File</a>"],["impl PyClass for <a class=\"struct\" href=\"mega_py/struct.FileDownload.html\" title=\"struct mega_py::FileDownload\">FileDownload</a>"],["impl PyClass for <a class=\"struct\" href=\"mega_py/struct.FolderEntry.html\" title=\"struct mega_py::FolderEntry\">FolderEntry</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[524]}
