44a0e3fda214cecee257625e6a4493f8284cc8c2
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2026-01-14 07:28:02 +0000

    deploy: 59ec96ab2ac18fe606c2e48734658ec3263a325a

---
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mega_cli",[["impl Subcommand for <a class=\"enum\" href=\"mega_cli/enum.Subcommand.html\" title=\"enum mega_cli::Subcommand\">Subcommand</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[146]}
